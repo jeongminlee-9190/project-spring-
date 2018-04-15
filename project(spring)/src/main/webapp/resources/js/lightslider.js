019adc83f43256e6109b5960ce1091f81923e545
han_148 <han@148@naver.com>
2018-04-16 01:18:36 +0900

    ss 0416 shopMemberView 작업 / slide이미지 작업 중

--- project(spring)/src/main/webapp/resources/js/lightslider.js
+++ project(spring)/src/main/webapp/resources/js/lightslider.js
@@ -1,11 +1,32 @@
 $(document).ready(function() {
-    var autoplaySlider = $('#autoplay').lightSlider({
+	
+    var slider = $('#slider').lightSlider({
+        item:3,
         auto:true,
         loop:true,
-        pauseOnHover: true,
-        onBeforeSlide: function (el) {
-            $('#current').text(el.getCurrentSlideCount());
-        } 
+        slideMove:1,
+		slideMargin:0,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed:600,
+		pager: false, 
+        responsive : [
+			{
+                breakpoint:420,
+                settings: {
+                    item:1,
+                    slideMove:1
+                  }
+            }
+        ]
     });
-    $('#total').text(autoplaySlider.getTotalSlideCount());
+	
+	$('#ev_arrow2').click(function(){
+        slider.goToNextSlide(); 
+    });
+	$('#ev_arrow1').click(function(){
+        slider.goToPrevSlide(); 
+    });
+    
+	
+	
 });
