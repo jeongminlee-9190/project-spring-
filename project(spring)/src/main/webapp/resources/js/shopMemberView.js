87f6d25c6a357275208b3fd270048804f599c396
han_148 <han@148@naver.com>
2018-04-18 01:08:15 +0900

    ss home 0417 shopMemberView 리뷰 작업 중

--- project(spring)/src/main/webapp/resources/js/shopMemberView.js
+++ project(spring)/src/main/webapp/resources/js/shopMemberView.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
 	
 	$("#shop_menu_title").on("click",function(){
-		$("#shop_menu_list_wrap").slideToggle("slow");
-		$("#shop_menu_title").find("img").fadeToggle("slow");
-		$("#shop_menu_up").find("img").fadeIn("slow");
+		$("#shop_menu_list_wrap").slideToggle(800);
+		$("#shop_menu_title").find("img").fadeToggle(200);
+		$("#shop_menu_up").css("margin-left","492px").fadeToggle(200);
 	});
 	
 	$("#shop_menu_up").on("click",function(){
-		$("#shop_menu_list_wrap").slideUp("slow");
-		$("#shop_menu_title").find("img").fadeIn("slow");
+		$("#shop_menu_title").find("img").fadeIn(200);
+		$("#shop_menu_list_wrap").slideUp(800);
+		$(this).fadeToggle(200);
 	});
 	
 	//사용자가 키워드 good, bad 클릭
@@ -41,6 +42,20 @@ $(document).ready(function(){
 	    $(".party").prop('checked',false);
 	    $(this).prop('checked',true);
 	  }
+	});
+	
+	$("#shop_review_cate_wrap input:checkbox").on("click",function(){
+		//console.log($(this).index("input:checked"));
+		var check = $(this).index("input:checked");
+		//var userChecked = check.is(":checked");
+		var labelbg = $($(this) + "label");
+		if(check==0){
+			$(labelbg).css("opacity","1");
+			console.log("check");
+		} else if (check==-1) {
+			$(labelbg).css("opacity","0.5");
+			console.log("Uncheck");
+		}
 	});
 	
 });
